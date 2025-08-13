89110fb12be36712af12dde7828e9bf20a8efbc1
Lavanya Gopinath <lgopinath@adobe.com>
2025-08-13 12:45:21 +0530

    Updated python

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
-class fruit:
-    def __init__(self):
-        self.color = ""
-
-apple = fruit("red") 
+try:
+    a = int(input()) #Step 1 -> getting input 10
+    b = int(input()) #Step 2 -> hi -> it gives error so, it go into except block
+    print(a+b)
+except Exception as e:
+    print("Something", e) #step 3 -> after coming to except block it will print "something"
